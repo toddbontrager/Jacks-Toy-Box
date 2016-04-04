a1623b04af77f5b9b841cf01c9efdbdc474a8d3f
Todd Bontrager <bontragertodd@gmail.com>
2016-04-04 18:30:41 -0400

    Move controller file into public folder

---
+++ public/controller.js
@@ -0,0 +1,6 @@
+angular.module('OrganizerApp', [])
+
+.controller('DaysController', function($scope){
+  $scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+})
