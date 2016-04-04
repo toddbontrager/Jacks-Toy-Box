dffa6205468c6f7b65dd545676453b14ce132955
Todd Bontrager <bontragertodd@gmail.com>
2016-04-04 19:40:18 -0400

    Allow click on each day to display static message

--- public/controller.js
+++ public/controller.js
@@ -2,16 +2,24 @@ angular.module('OrganizerApp', [])
 
 .factory('ToysFactory', function(){
   var toyStorage = {};
-  addToy.toyStorage = function(toy){
-    toyStorage[toy] = toy;
+  addOne.toyStorage = function(day, toy){
+    if (toyStorage[day] === undefined){
+      toyStorage[day] = [toy]
+    } else{
+    toyStorage[day].push(toy);
+    }
   }
   return toyStorage;
 })
 
 .controller('DaysController', function($scope){
   $scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 })
 
 .controller('ToysController', function($scope, ToysFactory){
-
+  $scope.toys = ToysFactory;
+  // $scope.addToy = function(){
+  //   ToysFactory.addOne($scope.??)
+  // }
 })
