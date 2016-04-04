f3c9943e50a9ae02cef9ca77fe009b6fce4535ee
Todd Bontrager <bontragertodd@gmail.com>
2016-04-04 18:11:23 -0400

    Add divs and h1 to html Include days in controller scope

--- controller.js
+++ controller.js
@@ -1,1 +1,6 @@
-angular.module('OrganizerApp')
+angular.module('OrganizerApp', [])
+
+.controller('ToysController', function($scope){
+  $scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+})
