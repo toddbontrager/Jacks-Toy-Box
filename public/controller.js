c4a9b235140f066fd6b906ecc4a8ccd23e7f6782
Todd Bontrager <bontragertodd@gmail.com>
2016-04-04 18:51:17 -0400

    Added: add toy form and button

--- public/controller.js
+++ public/controller.js
@@ -1,6 +1,17 @@
 angular.module('OrganizerApp', [])
 
+.factory('ToysFactory', function(){
+  var toyStorage = {};
+  addToy.toyStorage = function(toy){
+    toyStorage[toy] = toy;
+  }
+  return toyStorage;
+})
+
 .controller('DaysController', function($scope){
   $scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+})
+
+.controller('ToysController', function($scope, ToysFactory){
 
 })
