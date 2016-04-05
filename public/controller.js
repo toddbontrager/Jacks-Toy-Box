b8afe0054920d173bd603abb77b6a7f51ff1bedd
Todd Bontrager <bontragertodd@gmail.com>
2016-04-04 23:11:59 -0400

    Able to click on day and see toy
    Moved data into controller

--- public/controller.js
+++ public/controller.js
@@ -1,8 +1,16 @@
 angular.module('OrganizerApp', [])
 
 .factory('ToysFactory', function(){
-  var toyStorage = {};
-  addOne.toyStorage = function(day, toy){
+  var toyStorage = [
+    // {weekday: 'Sunday'}
+    // {weekday: 'Monday', toy: 'monkey'},
+    // {weekday: 'Tuesday', toy: 'horsey'},
+    // {weekday: 'Wednesday', toy: 'blocks'},
+    // {weekday: 'Thursday', toy: 'lawn mower'},
+    // {weekday: 'Friday', toy: 'music truck'},
+    // {weekday: 'Saturday', toy: 'books'}
+  ];
+  var addOne = function(day, toy){
     if (toyStorage[day] === undefined){
       toyStorage[day] = [toy]
     } else{
@@ -12,8 +20,34 @@ angular.module('OrganizerApp', [])
   return toyStorage;
 })
 
-.controller('DaysController', function($scope){
-  $scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+.controller('DaysController', function($scope, ToysFactory){
+  $scope.days =[
+  {weekday: 'Sunday', toy: 'ball'},
+  {weekday: 'Monday', toy: 'monkey'},
+  {weekday: 'Tuesday', toy: 'horsey'},
+  {weekday: 'Wednesday', toy: 'blocks'},
+  {weekday: 'Thursday', toy: 'lawn mower'},
+  {weekday: 'Friday', toy: 'music truck'},
+  {weekday: 'Saturday', toy: 'books'}
+  ];
+
+  // var toySelection = [];
+  // var daySelection = [];
+  
+  // angular.forEach(ToysFactory, function(value, key){
+  //  daySelection.push(key);
+  // });
+
+  // angular.forEach(ToysFactory, function(value){
+  //   toySelection.push(value);
+  // });
+
+  // $scope.days = daySelection;
+  //$scope.toys = toySelection;
+
+  //['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//   $scope.toys = ToysFactory.days
+//   //i need something so that on click, it passes the info of that particular day to the next function
 
 })
 
