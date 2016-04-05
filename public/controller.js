73be398fa93c82c9a74c306528bfec9c6b6ee799
Todd Bontrager <bontragertodd@gmail.com>
2016-04-05 13:36:14 -0400

    Change factory storage back to object

--- public/controller.js
+++ public/controller.js
@@ -1,14 +1,15 @@
 angular.module('OrganizerApp', [])
 
 .factory('ToysFactory', function(){
-  var toyStorage = {toys: [['toy1', 'toy2'], ['toy3', 'toy4']]};
-    // {weekday: 'Sunday'}
-    // {weekday: 'Monday', toy: 'monkey'},
-    // {weekday: 'Tuesday', toy: 'horsey'},
-    // {weekday: 'Wednesday', toy: 'blocks'},
-    // {weekday: 'Thursday', toy: 'lawn mower'},
-    // {weekday: 'Friday', toy: 'music truck'},
-    // {weekday: 'Saturday', toy: 'books'}
+  var toyStorage = {
+    Sunday: ['ball'],
+    Monday: ['horsey'],
+    Tuesday: ['blocks'],
+    Wednesday: ['lawn mower'],
+    Thursday: ['music truck'],
+    Friday: ['books'],
+    Saturday: ['monkey']
+  }
 
   var addOne = function(day, toy){
     if (toyStorage[day] === undefined){
@@ -21,34 +22,21 @@ angular.module('OrganizerApp', [])
 })
 
 .controller('DaysController', function($scope, ToysFactory){
-  var list = ToysFactory.toys;
-  console.log(list[0])
-  $scope.days =[
-  {weekday: 'Sunday', toy: list[0][0]},
-  {weekday: 'Monday', toy: 'monkey'},
-  {weekday: 'Tuesday', toy: 'horsey'},
-  {weekday: 'Wednesday', toy: 'blocks'},
-  {weekday: 'Thursday', toy: 'lawn mower'},
-  {weekday: 'Friday', toy: 'music truck'},
-  {weekday: 'Saturday', toy: 'books'}
-  ];
-
-  // var toySelection = [];
-  // var daySelection = [];
+  var toySelection = [];
+  var daySelection = [];
   
-  // angular.forEach(ToysFactory, function(value, key){
-  //  daySelection.push(key);
-  // });
+  angular.forEach(ToysFactory, function(value, key){
+   daySelection.push(key);
+  });
 
-  // angular.forEach(ToysFactory, function(value){
-  //   toySelection.push(value);
-  // });
+  angular.forEach(ToysFactory, function(value){
+    toySelection.push(value);
+  });
 
-  // $scope.days = daySelection;
-  //$scope.toys = toySelection;
+  $scope.days = daySelection;
+  $scope.toys = toySelection;
 
-  //['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-//   $scope.toys = ToysFactory.days
+
 //   //i need something so that on click, it passes the info of that particular day to the next function
 
 })
