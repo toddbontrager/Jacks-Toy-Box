cbb0b32bb5782674b7060fa716dd120435500717
Todd Bontrager <bontragertodd@gmail.com>
2016-04-05 13:50:04 -0400

    Moved controller inside public folder

--- controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('OrganizerApp', [])
-
-.controller('DaysController', function($scope){
-  $scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-})
