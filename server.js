bc6e6017bf35113813474d3883367f9ead4dac85
Todd Bontrager <bontragertodd@gmail.com>
2016-04-05 20:23:00 -0400

    Begin to make changes to switch to MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 4568;
 
+mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/toydb';
+mongoose.connect(mongoURI);
+
 app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+var Toy = mongoose.model('Toy', {
+  text: String
+})
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
