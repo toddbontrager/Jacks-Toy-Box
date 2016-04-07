4904085f5749abb7d87a6135e354478dff9254c3
Todd Bontrager <bontragertodd@gmail.com>
2016-04-06 23:57:38 -0400

    Add get and post api routes
    Prep for DB

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var morgan = require('morgan');
+// var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var wd = require('./server/weekdayModel.js');
+
 var port = process.env.PORT || 4568;
 
 var app = express();
 
 app.use(express.static('public'));
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 
@@ -15,6 +17,48 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+//Routes
+
+//grab all days from database
+app.get('/api/days', function(req, res, next){
+  wd.find(function(err, days){
+    if(err){
+      console.log('Error grabbing days from database: ' + err);
+      next(err);
+    }
+    console.log(days);
+    res.json(days);
+  });
+});
+
+app.post('/api/days', function(req, res, next){
+  wd.create({
+    day: req.body.name,
+    toys: req.body.toys
+  },
+  function(err, days){
+    if(err){
+      console.log('Error adding day/toy to database: ' + err);
+      next(err);
+    } else {
+      wd.find(function(err, days){
+        if(err){
+          next(err);
+        }
+        res.json(days);
+      });
+    }
+  });
+});
+
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
 });
+
+
+
+
+
+
+
+
